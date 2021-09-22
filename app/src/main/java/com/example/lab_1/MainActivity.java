9cc70a3a4f99540ec5835bf3fe03b77df6af0cb3
MeftahulIslam <turagmeftahul@gmail.com>
2021-09-22 21:50:48 +0600

    Second commit

--- app/src/main/java/com/example/lab_1/MainActivity.java
+++ app/src/main/java/com/example/lab_1/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.lab_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void btn1Click(View view) {
+        this.tvMain.setText("A wild text appears!");
     }
 }
